59de6641fb1d6ac5b96718b9d514f5827676442a
Manuel Sandoval <manusandoval05@gmail.com>
2023-08-23 15:13:43 -0600

    Better indexing in questions

--- src/routes/api/post-message/+server.ts
+++ src/routes/api/post-message/+server.ts
@@ -6,6 +6,11 @@ interface MessageFeed {
     role: "user" | "assistant",
 	content: string
 }
+interface EmbeddingData {
+    id: number,
+    content: string,
+    similarity: number
+}
 
 const openai = new OpenAI({
     apiKey: PRIVATE_OPENAI_KEY
@@ -19,67 +24,46 @@ export async function POST ({ request, locals: {supabase, getSession}}){
     const messages = body.messageFeed;
 
     if(!isMessageFeed(messages)){
-        throw error(400, "Body IS not formatted correctly");
+        throw error(400, "Body is not formatted correctly");
     }
     if(!session?.user){
        throw error(400, "User must be authenticated"); 
     }
 
-    
+    const userQuestion = messages.at(-1);
+
+    const embeddingResponse = await openai.embeddings.create({
+        model: "text-embedding-ada-002",
+        input: userQuestion ? userQuestion.content : ""
+    });
+    const embedding = embeddingResponse.data[0].embedding;
+
+    const { data } = await supabase.rpc("match_documents", {
+        query_embedding: embedding,
+        similarity_threshold: 0.6, 
+        match_count: 2
+    });
+    console.log(data);
+
     const completion = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         user: session.user.id,
         messages: [
             {
                 role: "system",
-                content: "Eres un asistente que contesta preguntas sobre un condominio en específico ubicado en el Estado de México, y por tanto está apegado a la ley general de condominios. Si la pregunta necesita de un fragmento en específico del reglamento, contesta 'Necesito: {una descripción del fragmento que necesitas para contestar la pregunta}'.  El usuario le va a proporcionar como respuesta el fragmento del reglamento que pidas entre 3 comillas simples. Con este fragmento contesta la pregunta del usuario. Si la pregunta del usuario no está relacionada con condominios, contesta 'No puedo contestar eso'"
+                content: "Eres un asistente que contesta preguntas sobre un condominio en específico ubicado en el Estado de México, y por tanto está apegado a la ley general de condominios. El usuario le va a proporcionar un fragmento de texto en forma de 'pregunta-respuesta' entre 3 comillas simples. Con ayuda de este fragmento contesta la pregunta del usuario sobre el condominio. Si la pregunta del usuario no está relacionada con condominios, contesta 'No puedo contestar eso'. Sé lo más conciso posible y evita decir frases como 'según el fragmento'"
             },
-            ...messages
+            ...messages,
+            {
+                role: "user",
+                content: `'''${data.map(embedding  => embedding.content).join(" ")}'''`
+            }
         ]
     });
 
     const chatResponse = completion.choices[0].message.content;
 
     console.log(chatResponse);
-
-    if(chatResponse?.startsWith("Necesito:")){
-        const embeddingResponse = await openai.embeddings.create({
-            model: "text-embedding-ada-002",
-            input: chatResponse
-        });
-
-        const embedding = embeddingResponse.data[0].embedding;
-
-        const { data } = await supabase.rpc("match_documents", {
-            query_embedding: embedding,
-            similarity_threshold: 0.6, 
-            match_count: 2
-        });
-
-        const completion = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo", 
-            user: session.user.id,
-            messages: [
-                {
-                    role: "system",
-                    content: "Eres un asistente que contesta preguntas sobre un condominio en específico ubicado en el Estado de México, y por tanto está apegado a la ley general de condominios. Si la pregunta necesita  de un fragmento en específico del reglamento, contesta 'Necesito: {una descripción del fragmento que necesitas}'.  El usuario le va a proporcionar como respuesta el fragmento del reglamento que pidas entre 3 comillas simples. Con este fragmento contesta la pregunta del usuario. Si la pregunta del usuario no está relacionada con condominios, contesta 'No puedo contestar eso'"
-                },
-                ...messages,
-                {
-                    role: "assistant",
-                    content: chatResponse
-                },
-                {
-                    role: "user",
-                    content: `'''${data[0].content}'''`
-                }
-            ]
-        });
-
-        return new Response(JSON.stringify({
-            prompt: completion.choices[0].message.content
-        }))
-    }
 
     return new Response(JSON.stringify({ 
         prompt: completion.choices[0].message.content,
